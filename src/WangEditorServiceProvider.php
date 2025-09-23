a7127a0429e962c7a98dfb0434735dec76e23649
jasonencode <2210843+jasonencode@users.noreply.github.com>
2025-09-23 01:29:46 +0000

    Fix styling

--- src/WangEditorServiceProvider.php
+++ src/WangEditorServiceProvider.php
@@ -20,7 +20,7 @@ class WangEditorServiceProvider extends PackageServiceProvider
     {
         $package->name(static::$name)
             ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function(InstallCommand $command) {
+            ->hasInstallCommand(function (InstallCommand $command) {
                 $command->publishConfigFile();
             });
 
@@ -39,9 +39,7 @@ class WangEditorServiceProvider extends PackageServiceProvider
         }
     }
 
-    public function packageRegistered(): void
-    {
-    }
+    public function packageRegistered(): void {}
 
     public function packageBooted(): void
     {
@@ -67,8 +65,8 @@ class WangEditorServiceProvider extends PackageServiceProvider
     protected function getAssets(): array
     {
         return [
-            Css::make('wang-editor-styles', __DIR__.'/../resources/dist/wang-editor.css'),
-            Js::make('wang-editor-scripts', __DIR__.'/../resources/dist/wang-editor.js'),
+            Css::make('wang-editor-styles', __DIR__ . '/../resources/dist/wang-editor.css'),
+            Js::make('wang-editor-scripts', __DIR__ . '/../resources/dist/wang-editor.js'),
         ];
     }
 
